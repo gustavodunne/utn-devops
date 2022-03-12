322f64d32c4e3b1c374a29840d19b8ec3c084f3a
Gustavo Dunne <55558685+gustavodunne@users.noreply.github.com>
2022-03-12 09:26:12 -0300

    Udpate de fuentes

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -48,7 +48,7 @@ if [ ! -d "$APP_PATH" ]; then
 	sudo mkdir /var/www
 	echo "clono el repositorio"
 	cd $APACHE_ROOT
-	sudo git clone https://github.com/Fichen/utn-devops-app.git
+	sudo git clone https://github.com/gusanomaly/utn-devops-app.git
 	cd $APP_PATH
 	sudo git checkout unidad-1
 fi
